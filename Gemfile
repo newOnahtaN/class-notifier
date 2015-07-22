3ea5e79b21df34086cb8da28d324010c0bf1495f
newOnahtaN <ncowen@email.wm.edu>
2015-07-22 00:44:29 -0400

    starting to gather class information

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
-gem 'pivotal-tracker'
 gem 'rails', '4.1.5'
 gem 'redis'
-
-
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'pry'
--- app/models/class_reporter.rb
+++ app/models/class_reporter.rb
@@ -1,9 +1,17 @@
+require 'capybara'
 class ClassReporter
 
   def find_classes
     #will generate @new_classes
     #use selenium	 
-    @new_classes = ["A new CS course"] 
+    @new_classes = ["A new CS course"]
+    session = Capybara::Session.new(:selenium)
+    session.visit "https://courselist.wm.edu/courselist/"
+    session.select("Computer Science", from: "term_subj")
+    node = session.find_field("term_code")
+    terms = node.all("*").map {|t| t.text}
+    Rails.logger.info terms
+    
   end
 
   def known_classes 
